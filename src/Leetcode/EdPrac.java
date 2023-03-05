663b30903ff55cf84656dc8c4379041d498f7309
elianbeyond <814846181@qq.com>
2023-03-05 15:10:02 +1100

    3.3 task1

--- src/Leetcode/EdPrac.java
+++ src/Leetcode/EdPrac.java
@@ -5,77 +5,28 @@ import java.util.*;
 public class EdPrac {
     public static void main(String[] args) {
         //TODO
-        //判断输入是否合法 Determine whether the input is legal
-        int num;
+        System.out.println("Enter two numbers: ");
         Scanner sc = new Scanner(System.in);
-        try{
-            num = sc.nextInt();
-        }catch (Exception e){
-            System.out.println("Error in input");
+        double a,b;
+        if(sc.hasNextDouble()){
+            a = sc.nextDouble();
+        }else {
+            System.out.println("Invalid input.");
             return;
         }
-        sc.nextLine();
-        boolean flag2 = false;
-        if (num <= 0) {
-            System.out.println("Error in input");
+        if(sc.hasNextDouble()){
+            b = sc.nextDouble();
+            sc.nextLine();
+        }else {
+            System.out.println("Invalid input.");
             return;
         }
-        if (num == 1 || num == 2) {
-            flag2 = true;
+        double c = (a+b)*b;
+        if(c==(a*a)){
+            System.out.println("Golden ratio!");
+        }else{
+            System.out.println("Maybe next time.");
         }
-        int i = 0;
-        boolean flag = false;
-
-
-        String str = sc.nextLine();
-        String[] arr = str.split(" ");
-        int[] ints = new int[arr.length];
-        if (arr.length != num) {
-            System.out.println("Invalid input");
-            return;
-        }
-
-        for (i = 0; i < arr.length; i++) {
-            try{
-                ints[i] = Integer.parseInt(arr[i]);
-            }catch (Exception e){
-                System.out.println("Invalid input");
-                return;
-            }
-            if (ints[i] < 0) {
-                flag = true;
-            }
-        }
-
-        if (flag) {
-            System.out.println("Invalid input");
-            return;
-        }
-        if (flag2) {
-            System.out.println("It is an Amazing Sequence");
-            return;
-        }
-
-
-        HashSet<Integer> difs = new HashSet<>();
-        for (i = 1; i < num; i++) {
-            if (ints[i] <= ints[i - 1] || ints[0]!=0) {
-                System.out.println("It is an ordinary sequence");
-                return;
-            }
-            for (int j = 0; j < i; j++) {
-                int tem = ints[i] - ints[j];
-                if (difs.contains(tem)) {
-                    System.out.println("It is an ordinary sequence");
-                    return;
-                }
-                difs.add(tem);
-
-
-            }
-        }
-        System.out.println("It is an Amazing Sequence");
-        return;
 
 
     }
